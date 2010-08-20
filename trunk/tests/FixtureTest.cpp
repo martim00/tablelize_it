253a6c13149175b7e5a4cc0f80d9fef0800f26f0
dablo19 <dablo19@c6af11b0-9e46-48d9-bc51-d12b312f76c5>
2010-08-20 16:53:51 +0000

    created folder for tests and separated
    git-svn-id: https://tablelize-it.svn.sourceforge.net/svnroot/tablelize-it@6 c6af11b0-9e46-48d9-bc51-d12b312f76c5

--- trunk/tests/FixtureTest.cpp
+++ trunk/tests/FixtureTest.cpp
@@ -1,37 +1,8 @@
 #include <igloo/igloo.h>
 using namespace igloo;
 
-#include "Fixture.h"
-#include "TableParser.h"
-#include "FluentInterfaceHelper.h"
-
-class CalculeIMC : public Fixture
-{
-public:
-   DECLARE_SUGAR(ParaPessoa, CalculeIMC);
-   DECLARE_SUGAR(Com, CalculeIMC);
-   DECLARE_SUGAR(E, CalculeIMC);
-   DECLARE_COLUMN(Altura, CalculeIMC);
-   DECLARE_COLUMN(Peso, CalculeIMC);
-   DECLARE_COLUMN(EsperaSe, CalculeIMC);   
-
-   void execute()
-   {   
-      Table *imcTable= GetTable("ImcTable");
-      for (unsigned i= 0; i < imcTable->GetRows().size(); i++)
-      {
-         double altura= this->GetCellAs<double>("ImcTable", "altura", i);
-         double peso= this->GetCellAs<double>("ImcTable", "peso", i);
-			double imcEsperado= this->GetCellAs<double>("ImcTable", "imc", i);
-			Assert::That(IMC(altura,peso)-imcEsperado,IsLessThan(0.01));     
-      }      
-   }
-
-	double IMC(double altura, double peso)
-	{
-		return peso / (altura*altura);
-	}
-};
+#include "..\Fixture.h"
+#include "..\TableParser.h"
 
 struct SimpleFixture : public Fixture
 {
@@ -41,25 +12,9 @@ struct SimpleFixture : public Fixture
    }
 };
 
-Context(SimpleExamples)
+Context(FixtureTest)
 {
-	Spec(Tokenizer)
-	{
-		TableParser parser;		
-		Table *table= parser.LoadTable("|ImcTable|\n|altura|peso|imc|\n|1.74|73|24.11|\n|1.63|62|23.33|\n|1.52|51|22.07|");
-		Assert::That(table->RowsCount(), Is().EqualTo(3));	
-      Assert::That(table->FieldsCount(), Is().EqualTo(3));	
-
-      delete table;
-	}   
-
-	Spec(ParserFromFile)
-	{		
-		Table *table= TableParser().LoadTableFromFile("teste.txt");
-		Assert::That(table->RowsCount(), Is().EqualTo(3));	
-      delete table;
-	}
-
+	
    Spec(ShouldReadExampleFromString)
    {
       SimpleFixture fixture;
@@ -83,32 +38,8 @@ Context(SimpleExamples)
       Assert::That(fixture.TablesCount(), Is().EqualTo(2));
 
       Assert::That(table->RowsCount(), Is().EqualTo(1));
-      Assert::That(otherTable->RowsCount(), Is().EqualTo(1));
-      
-   }
-
-   Spec(ShouldFailOnError)
-   {
-      CalculeIMC()
-         .ParaPessoa().Com()
-         .Peso("peso")
-         .E()
-         .Altura("altura")
-         .EsperaSe("imc")      
-         .LoadingDataFromFile("teste.txt");
-   }
-
-   Spec(ParserShouldAllowTableArgs)
-   {
-      TableParser parser;		
-      Table *table= parser.LoadTable("|TableWithArgs|someArg|\n"
-                                     "|field1|field2|\n"
-                                     "|1.74|73|\n\n");
-
-      Assert::That(table->GetTableArgAt(0), Is().EqualTo(std::string("someArg")));
-
-      delete table;
-   }
+      Assert::That(otherTable->RowsCount(), Is().EqualTo(1));      
+   }   
 
    Spec(GetTableShouldThrowIfThereIsMoreThanOneTableWithTheSameName)
    {
@@ -173,7 +104,6 @@ Context(SimpleExamples)
       
       Assert::That(tableWithArg->RowsCount(), Is().EqualTo(1));
       Assert::That(tableWithArg->GetCell("field1", 0), Is().EqualTo("1.74"));
-
 
       Table *otherTableWithArg= fixture.GetTableWithArg("TableWithArgs", 0, "otherArg");
 
---
+++ trunk/tests/TableParserTest.cpp
@@ -0,0 +1,34 @@
+#include <igloo/igloo.h>
+using namespace igloo;
+
+#include "..\TableParser.h"
+
+Context(TableParserTest)
+{
+	Spec(Tokenizer)
+	{
+		TableParser parser;		
+		Table *table= parser.LoadTable("|ImcTable|\n|altura|peso|imc|\n|1.74|73|24.11|\n|1.63|62|23.33|\n|1.52|51|22.07|");
+		Assert::That(table->RowsCount(), Is().EqualTo(3));	
+		Assert::That(table->FieldsCount(), Is().EqualTo(3));	
+
+		delete table;
+	}   
+
+	Spec(ParserFromFile)
+	{		
+		Table *table= TableParser().LoadTableFromFile("tests/TableParserTestContents.txt");
+		Assert::That(table->RowsCount(), Is().EqualTo(3));	
+		delete table;
+	}
+	
+	Spec(ParserShouldAllowTableArgs)
+	{
+		TableParser parser;		
+		Table *table= parser.LoadTable("|TableWithArgs|someArg|\n"
+			"|field1|field2|\n"
+			"|1.74|73|\n\n");
+		Assert::That(table->GetTableArgAt(0), Is().EqualTo(std::string("someArg")));
+		delete table;
+	}	
+};
