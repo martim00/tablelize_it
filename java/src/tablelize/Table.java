0c19a7da42353c9ed1d14e922153a03a23e654ec
martim <martim00@gmail.com>
2012-12-05 01:02:11 -0200

    porting to java...

--- java/src/tablelize/Table.java
+++ java/src/tablelize/Table.java
@@ -2,4 +2,19 @@ package tablelize;
 
 public class Table {
 
+	public void setPositionInFile(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setOriginFile(String originFile) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String tableName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
--- java/src/tablelize/TableParser.java
+++ java/src/tablelize/TableParser.java
@@ -1,36 +1,70 @@
 package tablelize;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Scanner;
 
 public class TableParser {
 
-	public Hashtable<String, ArrayList<Table>> LoadTables(String data) {
+	public Hashtable<String, ArrayList<Table>> LoadTables(String contents, String originFile) throws Exception {
 
-		Hashtable<String, ArrayList<Table>> tables;
-		std::stringstream ss(contents);      
-		std::string line, tableContents;
-		unsigned int order = 0;
-
-		while (std::getline(ss, line, '\n'))
-		{
-			if (!line.empty()) {
-				tableContents+= line;
-				tableContents+= "\n";
-
-			} else if (!tableContents.empty()) {
-				Table *loadedTable= LoadTable(tableContents);
-				loadedTable->SetPositionInFile(order++);
-				loadedTable->SetOriginFile(originFile);
-				tables.insert(std::pair<std::string, Table*>(loadedTable->TableName(), loadedTable));
-				tableContents.clear();
-			}
-		}
-
-		if (!contents.empty() && tables.empty())
-			throw std::runtime_error("There are contents to read but we cant parse a table. Maybe you forgot to put two empty lines at the end of the file");
-
+		Hashtable<String, ArrayList<Table>> tables = new Hashtable<String, ArrayList<Table>>();
+		
+		int order = 0;
+		String tableContents = new String();		
+		
+		try (Scanner scanner =  new Scanner(contents)) {
+	      while (scanner.hasNextLine()) {
+	        //process each line in some way
+	        String line = scanner.nextLine();
+	        
+	        if (!line.isEmpty()) {
+	        	
+	        	tableContents+= line;
+	        	tableContents+= "\n";
+	        	
+	        } else if (!tableContents.isEmpty()) {
+	        	
+	        	Table loadedTable = LoadTable(tableContents);
+				loadedTable.setPositionInFile(order++);
+				loadedTable.setOriginFile(originFile);
+				tables.get(loadedTable.tableName()).add(loadedTable);
+				tableContents = "";
+	        }
+	      }
+	    }
+		
+		if (!contents.isEmpty() && tables.isEmpty())
+			throw new Exception("There are contents to read but we cant parse a table. Maybe you forgot to put two empty lines at the end of the file");
+		
 		return tables;
+	}
+
+	private Table LoadTable(String tableContents) {
+		
+//		Scanner scanner = new Scanner(tableContents);
+//		
+//		Table table = parseTableName(scanner.nextLine());
+//		String header = scanner.nextLine();
+//		
+//		
+//		std::stringstream stream(contents.c_str());
+//
+//		Table *table= parseTableName(getLine(stream));
+//
+//		std::string header= getLine(stream);
+//		std::vector<std::string> fields= tokenize(header, '|');	
+//
+//		while (!stream.eof())
+//		{
+//			std::string line= getLine(stream);
+//			if (line.empty()) continue;
+//			std::vector<std::string> lineParams= tokenize(line, '|');
+//			addParametersInTable(*table, fields, lineParams); 
+//		}	   
+//		return table;    
 
 	}
 
